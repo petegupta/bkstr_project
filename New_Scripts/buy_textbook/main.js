38f356ac0729490d7319d549fceebbe03873362b
Arpit <arpit.jha@ucertify.com>
2023-10-13 09:30:27 +0530

    update

--- New_Scripts/buy_textbook/main.js
+++ New_Scripts/buy_textbook/main.js
@@ -163,7 +163,7 @@ async function getDept(campusId, termId) {
         if (err) {
           reject(`XML parsing error: ${err.message}`);
         }
-        const departments = result.departments.department.map(department => ({
+        const departments = result?.departments?.department.map(department => ({
           id: department.$.id,
           abrev: department.$.abrev,
           name: department.$.name
@@ -195,7 +195,7 @@ async function getCourse(deptId, termId) {
         if (err) {
           reject(`XML parsing error: ${err.message}`);
         }
-        const coursesArray = result.courses.course.map(course => ({
+        const coursesArray = result?.courses?.course.map(course => ({
           id: course.$.id,
           name: course.$.name,
         }));
@@ -226,7 +226,7 @@ async function getSection(courseId, termId) {
         if (err) {
           reject(`XML parsing error: ${err.message}`);
         }
-        const sectionsArray = result.sections.section.map(section => ({
+        const sectionsArray = result?.sections?.section.map(section => ({
           id: section.$.id,
           name: section.$.name,
           instructor: section.$.instructor
