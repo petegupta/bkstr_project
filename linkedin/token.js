dd5554d7c661b1f1ab5f562d3c931741365785d4
uc-amz <mohd.amzad@ucertify.com>
2024-06-03 14:58:58 +0530

    update

---
+++ linkedin/token.js
@@ -0,0 +1,19 @@
+fetch("https://api.getprospect.com/api/v1/insights/search/contact?linkedinUrl=https:%2F%2Fwww.linkedin.com%2Fin%2Fbrowna4234", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImxlaGFiaUBjbGlwLmxhdCIsImZpcnN0TmFtZSI6IkFzcnQiLCJsYXN0TmFtZSI6IlNhbGYiLCJlbWFpbFZlcmlmaWVkIjp0cnVlLCJwaG9uZVZlcmlmaWVkIjpmYWxzZSwiaGFzUmlza1Njb3JlIjpmYWxzZSwiaGFzTGltaXRlZFBsYW4iOmZhbHNlLCJ3b3Jrc3BhY2VJZCI6NTQ2MTkyLCJpc0FkbWluIjp0cnVlLCJxdWFsaWZpY2F0aW9uIjpmYWxzZSwiaXNUZW1wIjpmYWxzZSwiX2lkIjoiNjY1ZDg2N2NlYzAxZDMxYTMxOGFiMzgxIiwic2hvd0xlYWRUb3VyIjpmYWxzZSwidGF4IjowLCJpYXQiOjE3MTc0MDUzMjAsImV4cCI6MTcyMTk4OTMyMH0.t3hbmz4kS3PtfNDa4_Mit9BAXBpX6VlI8-cLbLSvEVY",
+    "if-none-match": "W/\"839-GN/ja8Mrw4j9iSRyds2EMNxM9oA\"",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "Referer": "https://app.getprospect.com/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});
